aefccc857bb7d193d391bf26d3f3442409fda8ad
Herce, Alejandro(aherce) <aherce@paypal.com>
2018-05-09 15:44:18 -0500

    More stuff

--- src/server/controllers/users.js
+++ src/server/controllers/users.js
@@ -1,10 +1,33 @@
 const users = require('../models/users');
+const nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'pypltestmx@gmail.com',
+    pass: 'Ebay2017pypl'
+  }
+});
+
+var welcomeMailOptions = {
+  from: '"marXel 👻" <pypltestmx@gmail.com>',
+  subject: 'Welcome to marXel',
+}
 
 exports.signup = function(req, res) {
   users.signup(req.body, function(err, data) {
     if (err) {
       res.status(400).send({error: err});
     } else {
+      welcomeMailOptions.to = '' + req.body.email;
+      welcomeMailOptions.html = '<h1>Welcome to marXel ' + req.body.name + '!</h1><p>Your login information is:</p><p>Email: ' + req.body.email + '</p><p>Password: ' + req.body.password + '</p>';
+      transporter.sendMail(welcomeMailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
       res.status(201).send(data);
     }
   })
